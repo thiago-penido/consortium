a8b617987f897fed83d045223f215c7d071d2c02
thiagopenido <tpenido@thoughtworks.com>
2017-01-10 20:19:14 -0200

    Thiago/Gustavo Started to design a server side

--- src/main/java/com/avepe/controllers/ClientController.java
+++ src/main/java/com/avepe/controllers/ClientController.java
@@ -4,25 +4,37 @@ import com.avepe.models.Client;
 import com.avepe.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+@Controller
 @RequestMapping("client")
 public class ClientController {
 
     @Autowired
     ClientService clientService;
 
+    @ResponseBody
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Client> saveClient(Client client) {
         return new ResponseEntity<>(clientService.saveClient(client), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<Client> getClient() {
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, produces = APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Client> getClientJson() {
         return new ResponseEntity<>(clientService.getClient(), HttpStatus.OK) ;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getClientHtml() {
+        return new ModelAndView("client/show", "client", clientService.getClient());
     }
 }
